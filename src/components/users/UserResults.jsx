e174a3572c75a8c5cb6ee0cae153734278d3496a
antor <antorinfo@gmail.com>
2022-02-05 11:10:34 +0600

    first commit

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import Spinner from '../layout/Spinner';
+import UserItem from './UserItem';
+import GithubContext from '../../context/github/GithubContext';
+
+function UserResults() {
+  const { users, loading } = useContext(GithubContext);
+
+  if (!loading) {
+    return (
+      <div className='grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+        {users?.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
+}
+
+export default UserResults;
